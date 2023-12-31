8f1286c4fcb62e0ec09237c0170f66ea6e6a8d36
bahadirmaktav <bahadirmaktavv@gmail.com>
2023-12-31 03:59:06 +0300

    Improve software controller gui

--- software/src/ui_manager.py
+++ software/src/ui_manager.py
@@ -11,51 +11,73 @@ class UiManager(tk.Tk):
     self.event_loop = event_loop
     self.command_manager = command_manager
 
-    # set window title
+    # Set Window Title
     self.title("TEOS CONTROLLER")
 
-    # set window size
-    width=1000
-    height=600
+    # Set Window Size
+    self.resolution = 20
+    self.width = self.resolution * 51
+    self.height = self.resolution * 27
     screenwidth = self.winfo_screenwidth()
     screenheight = self.winfo_screenheight()
-    alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+    alignstr = '%dx%d+%d+%d' % (self.width, self.height, (screenwidth - self.width) / 2, (screenheight - self.height) / 2)
     self.geometry(alignstr)
     self.resizable(width=False, height=False)
 
+    # Control Display Seperation Line
+    self.seperation_canvas = tk.Canvas(self, width=self.width, height=self.height)
+    self.seperation_canvas.place(x=0, y=0)
+    self.seperation_canvas.create_line(self.resolution * 17, 0, self.resolution * 17, self.height, fill="black")
+
     # Camera Controls
-    self.camera_controls_label = tk.Label(text="Camera Controls")
-    self.camera_controls_label.place(x=0, y=0, width=150, height=40)
-    self.start_camera_button = tk.Button(text="Start Camera", command=lambda: self.event_loop.create_task(self.start_camera()))
-    self.start_camera_button.place(x=0, y=45, width=100, height=40)
-    self.stop_camera_button = tk.Button(text="Stop Camera", command=lambda: self.event_loop.create_task(self.stop_camera()))
-    self.stop_camera_button.place(x=105, y=45, width=100, height=40)
+    self.camera_canvas = tk.Canvas(self, width=self.resolution*15, height=self.resolution*5)
+    self.camera_canvas.place(x=self.resolution, y=self.resolution)
+    self.camera_canvas.create_rectangle(0, 0, self.resolution*15, self.resolution*5, fill="gray", outline="black")
+    self.camera_controls_label = tk.Label(self.camera_canvas, text="Camera Controls")
+    self.camera_controls_label.place(x=75, y=10, width=150, height=38)
+    self.start_camera_button = tk.Button(self.camera_canvas, text="Start Camera", command=lambda: self.event_loop.create_task(self.start_camera()))
+    self.start_camera_button.place(x=48, y=58, width=100, height=32)
+    self.stop_camera_button = tk.Button(self.camera_canvas, text="Stop Camera", command=lambda: self.event_loop.create_task(self.stop_camera()))
+    self.stop_camera_button.place(x=152, y=58, width=100, height=32)
 
     # Motor Controls
-    self.motor_controls_label = tk.Label(text="Motor Controls")
-    self.motor_controls_label.place(x=0, y=105, width=150, height=40)
-    self.angle = tk.Entry(self)
-    self.angle.place(x=0, y=150, width=100, height=40)
-    self.angle.bind('<KeyRelease>', self.validate_positive_float)
-    self.pan_rotate = tk.Button(text="Rotate Pan", command=lambda: self.event_loop.create_task(self.rotate_pan()))
-    self.pan_rotate.place(x=105, y=150, width=100, height=40)
-    self.tilt_rotate = tk.Button(text="Rotate Tilt", command=lambda: self.event_loop.create_task(self.rotate_tilt()))
-    self.tilt_rotate.place(x=210, y=150, width=100, height=40)
+    self.motor_canvas = tk.Canvas(self, width=self.resolution*15, height=self.resolution*7)
+    self.motor_canvas.place(x=self.resolution, y=self.resolution*7)
+    self.motor_canvas.create_rectangle(0, 0, self.resolution*15, self.resolution*7, fill="gray", outline="black")
+    self.motor_controls_label = tk.Label(self.motor_canvas, text="Motor Controls")
+    self.motor_controls_label.place(x=75, y=10, width=150, height=38)
+    self.motor_controls_label = tk.Label(self.motor_canvas, text="Pan")
+    self.motor_controls_label.place(x=15, y=58, width=50, height=32)
+    self.pan_slider = tk.Scale(self.motor_canvas, from_=0, to=100, orient=tk.HORIZONTAL, command=lambda value: self.event_loop.create_task(self.rotate_pan(value)))
+    self.pan_slider.place(x=85, y=58, width=200, height=32)
+    self.motor_controls_label = tk.Label(self.motor_canvas, text="Tilt")
+    self.motor_controls_label.place(x=15, y=98, width=50, height=32)
+    self.pan_slider = tk.Scale(self.motor_canvas, from_=0, to=100, orient=tk.HORIZONTAL, command=lambda value: self.event_loop.create_task(self.rotate_tilt(value)))
+    self.pan_slider.place(x=85, y=98, width=200, height=32)
 
     # WebSocket Connection Controls
-    self.websocket_connection_controls_label = tk.Label(text="WebSocket Connection Controls")
-    self.websocket_connection_controls_label.place(x=0, y=210, width=250, height=40)
-    self.connect_ws_button = tk.Button(text="Connect", command=lambda: self.event_loop.create_task(self.connect_websocket_server()))
-    self.connect_ws_button.place(x=0, y=255, width=100, height=40)
-    self.disconnect_ws_button = tk.Button(text="Disconnect", command=lambda: self.event_loop.create_task(self.disconnect_websocket_server()))
-    self.disconnect_ws_button.place(x=105, y=255, width=100, height=40)
+    self.websocket_canvas = tk.Canvas(self, width=self.resolution*15, height=self.resolution*5)
+    self.websocket_canvas.place(x=self.resolution, y=self.resolution*15)
+    self.websocket_canvas.create_rectangle(0, 0, self.resolution*15, self.resolution*5, fill="gray", outline="black")
+    self.websocket_connection_controls_label = tk.Label(self.websocket_canvas, text="WebSocket Connection Controls")
+    self.websocket_connection_controls_label.place(x=25, y=10, width=250, height=38)
+    self.connect_ws_button = tk.Button(self.websocket_canvas, text="Connect", command=lambda: self.event_loop.create_task(self.connect_websocket_server()))
+    self.connect_ws_button.place(x=48, y=58, width=100, height=32)
+    self.disconnect_ws_button = tk.Button(self.websocket_canvas, text="Disconnect", command=lambda: self.event_loop.create_task(self.disconnect_websocket_server()))
+    self.disconnect_ws_button.place(x=152, y=58, width=100, height=32)
 
     # Mode Controls
+    self.mode_canvas = tk.Canvas(self, width=self.resolution*15, height=self.resolution*5)
+    self.mode_canvas.place(x=self.resolution, y=self.resolution*21)
+    self.mode_canvas.create_rectangle(0, 0, self.resolution*15, self.resolution*5, fill="gray", outline="black")
     # TODO(MBM): Implement later.
 
     # Image Display
+    self.display_canvas = tk.Canvas(self, width=self.resolution*16, height=self.resolution*12)
+    self.display_canvas.place(x=self.resolution*26, y=self.resolution*7.5)
+    self.display_canvas.create_rectangle(0, 0, self.resolution*16, self.resolution*12, outline="black")
     self.image_label = tk.Label(self)
-    self.image_label.place(x=500, y=150, width=400, height=400)
+    self.image_label.place(x=self.resolution*26, y=self.resolution*7.5, width=self.resolution*16, height=self.resolution*12)
 
   async def show(self):
     while True:
@@ -70,13 +92,13 @@ class UiManager(tk.Tk):
     logger.info("Stop camera button clicked.")
     await self.command_manager.stop_camera()  
 
-  async def rotate_pan(self):
+  async def rotate_pan(self, value):
     logger.info("Rotate pan button clicked.")
-    await self.command_manager.rotate_motor(Motor.PAN, float(self.angle.get()))
+    await self.command_manager.rotate_motor(Motor.PAN, float(value))
 
-  async def rotate_tilt(self):
+  async def rotate_tilt(self, value):
     logger.info("Rotate tilt button clicked.")
-    await self.command_manager.rotate_motor(Motor.TILT, float(self.angle.get()))
+    await self.command_manager.rotate_motor(Motor.TILT, float(value))
 
   async def connect_websocket_server(self):
     logger.info("Connect to websocket server button clicked.")
